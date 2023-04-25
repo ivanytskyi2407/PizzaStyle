27fec8e585ea07f49097865af43665aa544648b6
Oleh Ivanytskyi <89008107+ivanytskyi2407@users.noreply.github.com>
2023-04-25 16:31:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="pizzaStyle">
-     */}
     <Provider store={store}>
       <BrowserRouter basename="PizzaStyle">
         <App />
