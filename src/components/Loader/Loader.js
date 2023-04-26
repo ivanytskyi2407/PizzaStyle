089c5ff9397ea738562989254ff023a3712daa74
Oleh Ivanytskyi <89008107+ivanytskyi2407@users.noreply.github.com>
2023-04-26 23:15:33 +0300

    -

--- src/components/Loader/Loader.js
+++
@@ -1,8 +0,0 @@
-import { BallTriangle } from 'react-loader-spinner';
-import s from './Loader.module.css';
-
-export const Loader = () => (
-  <div className={s.Loader}>
-    <BallTriangle height="100" width="100" color="blue" ariaLabel="loading" />
-  </div>
-);
